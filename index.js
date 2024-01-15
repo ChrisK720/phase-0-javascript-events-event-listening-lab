e40133cc257634a9061b60cb821f0ea4e4555974
Christopher Kirathe <chrisk720@Christophers-MacBook-Pro.local>
2024-01-15 12:08:20 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+function addingEventListener(color) {
+   let div = document.getElementById("input")
+   div.addEventListener('click', (event) => {
+    div.style.color = color
+
+   })
 }
+
+addingEventListener('red')
+
+
+
+
+
